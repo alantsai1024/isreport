969de18df1ba6eea9fcdec0a7638a51c2715c3d9
alantsai1024 <92162261+alantsai1024@users.noreply.github.com>
2024-06-09 17:23:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -79,6 +79,4 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/router'))
 app.use('/auth', require('./routes/auth'))
 
-app.listen(5555, () => {
-    console.log(`服務運行在：http://localhost:5555}`);
-})
+module.exports = app;
